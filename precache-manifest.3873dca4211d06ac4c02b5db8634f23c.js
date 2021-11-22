b57e487a98ac3a55cac4c9b81d6d42ffa9b810fa
Lucas Duarte <lucs.rkt@gmail.com>
2021-11-22 19:15:05 -0300

    Updates

---
+++ precache-manifest.3873dca4211d06ac4c02b5db8634f23c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d620439a7e4d91e4d6098836f35ca98",
+    "url": "/recipes-app/index.html"
+  },
+  {
+    "revision": "6bdb88116d91a089fbdc",
+    "url": "/recipes-app/static/css/2.429e79c4.chunk.css"
+  },
+  {
+    "revision": "bf1d56b4bcbaa2310083",
+    "url": "/recipes-app/static/css/main.97a34b73.chunk.css"
+  },
+  {
+    "revision": "6bdb88116d91a089fbdc",
+    "url": "/recipes-app/static/js/2.ae9d7dad.chunk.js"
+  },
+  {
+    "revision": "cf5fb949fa332cc8300c73eca95dbfdb",
+    "url": "/recipes-app/static/js/2.ae9d7dad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf1d56b4bcbaa2310083",
+    "url": "/recipes-app/static/js/main.c0ec38b2.chunk.js"
+  },
+  {
+    "revision": "25993f9b630f63b44851",
+    "url": "/recipes-app/static/js/runtime-main.1228032d.js"
+  },
+  {
+    "revision": "0ad0955fd902c3beee4006bda8a5877a",
+    "url": "/recipes-app/static/media/logo.0ad0955f.png"
+  },
+  {
+    "revision": "06215ed4cae23455c02abc804445ba74",
+    "url": "/recipes-app/static/media/shareIcon.06215ed4.svg"
+  }
+]);
